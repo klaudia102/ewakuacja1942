7b0d7d60733cc3d890571a2d701143dba5d10eff
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-03-28 20:51:48 +0000

    added react router and basic nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 
-import './App.css'
+import './App.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
+import Home from './pages/Home';
+import Search from './pages/search';
+import History from './pages/History';
+import Contact from './pages/Contact';
+
 
 function App() {
-
   return (
-    <>
-   
-      <h1>Ewakuowani 1942</h1>
-      <h2>lista ewakowanych z ZSRR w 1942</h2>
-      <p> Tu bedzie teks opisujący historię ewakuacji</p>
+<>
+    <div>
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/history" element={<History />} />
+          </Routes>
+        </div>
+      </Router>
       
+    </div>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
