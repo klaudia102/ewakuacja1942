76ef0a74d5e09f79ff49de53cd723e7cd78bf8a8
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-03-28 20:07:15 +0000

    added functions and importing style.css to pages

--- src/pages/history/index.jsx
+++ src/pages/history/index.jsx
@@ -1,14 +1,16 @@
-// Copyright 2024 klaud
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     https://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+import './style.css'
 
+function History() {
+
+  return (
+    <>
+   
+      <h1>Ewakuowani 1942</h1>
+      <h2>lista ewakowanych z ZSRR w 1942</h2>
+      <p> Tu bedzie historia ewakuacji </p>
+      
+    </>
+  )
+}
+
+export default History
