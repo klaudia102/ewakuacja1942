48250c67c8d0508e158b6534397939b02107070a
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-03-28 20:14:41 +0000

    added contact page and react router

---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+
+function Contact() {
+
+  return (
+    <>
+   
+      <h1>Ewakuowani 1942</h1>
+      <h2>lista ewakowanych z ZSRR w 1942</h2>
+      <p> Tu bedzie mozliwosc kontaktu</p>
+      
+    </>
+  )
+}
+
+export default Contact
