474084604761b8163cbe81627d085b0e9ac43dbb
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-03-29 21:28:37 +0000

    added jumbotron function to home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,11 @@
 import './style.css'
+import Jumbotron from '../../components/Jumbotron'
 
 function Home() {
 
   return (
     <>
-   
+   < Jumbotron />
       <h1>Ewakuowani 1942</h1>
       <h2>lista ewakowanych z ZSRR w 1942</h2>
       <p> Tu bedą strona domowa</p>
