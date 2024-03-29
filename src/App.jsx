cd6ccf7dd48355e3a09c07e05cf789292d35603d
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-03-29 07:53:19 +0000

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css';
-
+import * as bootstrap from 'bootstrap';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header';
 import Home from './pages/Home';
