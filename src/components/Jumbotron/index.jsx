422699a80fe4be148413e3debcac1d552572f8cf
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-04-10 18:44:37 +0100

    change to text in jumbotron

--- src/components/Jumbotron/index.jsx
+++ src/components/Jumbotron/index.jsx
@@ -4,10 +4,14 @@ import './style.css';
 function Jumbotron() {
     return (
         <div>
-            <div class="jumbotron jumbotron-fluid jumbotron_container">
-                <div class="container">
-                    <h1 class="display-4">Ewakuowani 1942</h1>
-                    <h4 class="lead">Lista żołnierzy ewakuowanych z ZSRR w 1942 r.</h4>
+            <div className="jumbotron jumbotron-fluid jumbotron_container">
+                <div className="container">
+                    <div className="container_header">
+                        <h1 className="display-4">Ewakuowani 1942</h1>
+                    </div>
+                    <div className="container_header">
+                        <h4 class="lead">Lista żołnierzy ewakuowanych z ZSRR w 1942 r.</h4>
+                    </div>
                 </div>
             </div>
             <p1>Fotografia:Narodowe Archiwum Cyfrowe</p1>
