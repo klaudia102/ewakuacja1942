72f6db95be046a00e1f791c96e6474e61a267c4a
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-04-20 20:52:19 +0100

    added code for findings

--- src/components/Findings/index.jsx
+++ src/components/Findings/index.jsx
@@ -1,14 +1,21 @@
 import './style.css'
+import React from 'react';
 
-function Findings() {
-
+function Findings(props) {
   return (
     <>
-   
-      <h1>Ewakuowani 1942</h1>
-      <h2>lista ewakowanych z ZSRR w 1942</h2>
-      <p> Tu bedą wyniki wyszukiwania</p>
-      
+<section className='card_container'> 
+ <div className='card'> 
+      <h4 className="card_title">imię i nazwisko: {props.imie} {props.nazwisko}</h4>
+      <p>data urodzenia: {props.urodzony}</p>
+      <p>miejsce urodzenia: {props.miejsceUr}</p>
+      <p>stopień wojskowy: {props.stopień}</p>
+      <p>przydział w ZSRR: {props.stopień}</p>
+      <p>data ewakuacji: {props.data}</p>
+      <p>przydział na Środkowym Wschodzie: {props.nowyPrzydział}</p>
+      <p>sygnatury archiwalne: {props.sygnatura}</p>
+      </div>
+</section>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
-import * as bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
