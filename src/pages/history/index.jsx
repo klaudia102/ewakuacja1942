df7ab663348e4d330f4d16e09869478cc350613b
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-12-21 20:06:13 +0000

    added first note to home webpage

--- src/pages/history/index.jsx
+++ src/pages/history/index.jsx
@@ -1,12 +1,11 @@
 import './style.css'
+import MainHeader from '../../mainHeader'
 
 function History() {
 
   return (
     <>
-   
-      <h1>Ewakuowani 1942</h1>
-      <h2>lista ewakowanych z ZSRR w 1942</h2>
+   < MainHeader />
       <p> Tu bedzie historia ewakuacji </p>
       <div>
 
