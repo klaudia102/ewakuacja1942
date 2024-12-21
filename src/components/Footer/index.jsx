2ae42b9d30edb6f344853ac88e56c18dc4c4e558
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-12-21 18:40:55 +0000

    added changes to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,6 +8,9 @@ function Footer() {
                 <p>Kontact</p>
                 <p1>ewakuowani1942@gmail.com</p1>
             </div>
+            <div className='footer_logo'>
+            <img src="/Ewakuowani(5).png" alt="ewakuowani1942 logo" height="90px" />
+            </div>
             <div className='copy_rights_container'>
                 <p>Prawa autorskie do strony</p>
                 <a href='https://klaudia102.github.io/Klaudia-portfolio-react/'>Klaudia Kierepka</a>
