7437779e1494ae0b53481eee4f28c85adf0b07d4
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-12-21 20:16:46 +0000

    further changes to footer - link do home in logo

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,8 +9,13 @@ function Footer() {
             <div className='contact_link'>
 <NavLink to='/contact' > Kontakt </NavLink> 
            </div>
-            <div className='footer_logo'>
-            <img src="/Ewakuowani(5).png" alt="ewakuowani1942 logo" height="90px" />
+            <div className='footer_logo'> 
+            <NavLink to='/' >
+            <a href="#">
+              <img src="/Ewakuowani(5).png" alt="ewakuowani1942 logo" height="90px" />
+            </a>
+            </NavLink> 
+            {/* <img src="/Ewakuowani(5).png" alt="ewakuowani1942 logo" height="90px"  /> */}
             </div>
             <div className='copy_rights_container'>
                 <p>Prawa autorskie do strony</p>
